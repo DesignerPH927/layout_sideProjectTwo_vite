fb2357ef0d500a35872930d7e8980e8879bfcc50
DesignerPH927 <designer.ph927@gmail.com>
2023-10-06 23:42:04 +0800

    change changeBoxJs

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import './assets/scss/all.scss';
 // btnToggle
 import './javaScripts/btnToggle.js';
 
+// changeBox
+import './javaScripts/changeBox.js';
+
 // openModal
 import './javaScripts/openModal.js';
 
