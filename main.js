f9b62831bf10fd87f6ec22dba25e404ebed2500c
DesignerPH927 <designer.ph927@gmail.com>
2023-10-21 13:44:35 +0800

    change JS 1021

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 import './assets/scss/all.scss';
 
-
 // btnToggle
 import './javaScripts/btnToggle.js';
-
-// changeBox
-// import './javaScripts/changeBox.js';
-
 // openModal
 // import './javaScripts/openModal.js';
-
+// changeBox
+// import './javaScripts/changeBox.js';
 // rotateArrow
 // import './javaScripts/rotateArrow.js';
-
